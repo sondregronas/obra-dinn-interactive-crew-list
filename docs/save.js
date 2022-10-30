b4054c1a3ba1500d56588eb8ec523c7958854b12
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-10-30 22:40:57 +0100

    Add menu buttons for sketch & savestate

---
+++ docs/save.js
@@ -0,0 +1,11 @@
+function save() {
+    alert('Sorry, not implemented yet!');
+}
+
+function load() {
+    alert('Sorry, not implemented yet!');
+}
+
+function reset() {
+    alert('Sorry, not implemented yet!');
+}
