fd583758f426844ce7b96961ac505d875663680f
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-10-31 22:19:09 +0100

    :book: Add instructions & reset portrait feature

--- docs/modals.js
+++ docs/modals.js
@@ -1,16 +1,19 @@
 // Modals
+const helpModal = document.getElementById("help-modal");
 const crewModal = document.getElementById("crew-modal");
 const saveModal = document.getElementById("save-modal");
 const loadModal = document.getElementById("load-modal");
 
 
 // Open modal
+document.getElementById("helpBtn").onclick = function() {helpModal.style.display = "block";}
 document.getElementById("crewBtn").onclick = function() {selectedPortrait = null; crewModal.style.display = "block";}
 document.getElementById("saveBtn").onclick = function() {generateSaveState(); saveModal.style.display = "block";}
 document.getElementById("loadBtn").onclick = function() {loadModal.style.display = "block";}
 
 
 // Close button
+document.getElementsByClassName("helpClose")[0].onclick = function() {helpModal.style.display = "none";}
 document.getElementsByClassName("crewClose")[0].onclick = function() {crewModal.style.display = "none";}
 document.getElementsByClassName("saveClose")[0].onclick = function() {saveModal.style.display = "none";}
 document.getElementsByClassName("loadClose")[0].onclick = function() {loadModal.style.display = "none";}
@@ -18,6 +21,7 @@ document.getElementsByClassName("loadClose")[0].onclick = function() {loadModal.
 
 // Close modals when user clicks outside
 window.onclick = function(event) {
+    if (event.target === helpModal) {helpModal.style.display = "none";}
     if (event.target === crewModal) {crewModal.style.display = "none";}
     if (event.target === saveModal) {saveModal.style.display = "none";}
     if (event.target === loadModal) {loadModal.style.display = "none";}
