ed7c77aa19558aab51b76793aa6727a65dd47999
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-11-14 11:05:20 +0100

    :fire: Reset Button!

--- docs/modals.js
+++ docs/modals.js
@@ -3,6 +3,7 @@ const helpModal = document.getElementById("help-modal");
 const crewModal = document.getElementById("crew-modal");
 const saveModal = document.getElementById("save-modal");
 const loadModal = document.getElementById("load-modal");
+const resetModal = document.getElementById("reset-modal");
 
 
 // Open modal
@@ -10,6 +11,7 @@ document.getElementById("helpBtn").onclick = function() {helpModal.style.display
 document.getElementById("crewBtn").onclick = function() {selectedPortrait = null; crewModal.style.display = "block";}
 document.getElementById("saveBtn").onclick = function() {generateSaveState(); saveModal.style.display = "block";}
 document.getElementById("loadBtn").onclick = function() {loadModal.style.display = "block";}
+document.getElementById("resetBtn").onclick = function() {resetModal.style.display = "block";}
 
 
 // Close button
@@ -17,6 +19,7 @@ document.getElementsByClassName("helpClose")[0].onclick = function() {helpModal.
 document.getElementsByClassName("crewClose")[0].onclick = function() {crewModal.style.display = "none";}
 document.getElementsByClassName("saveClose")[0].onclick = function() {saveModal.style.display = "none";}
 document.getElementsByClassName("loadClose")[0].onclick = function() {loadModal.style.display = "none";}
+document.getElementsByClassName("resetClose")[0].onclick = function() {resetModal.style.display = "none";}
 
 
 // Close modals when user clicks outside
@@ -25,6 +28,7 @@ window.onclick = function(event) {
     if (event.target === crewModal) {crewModal.style.display = "none";}
     if (event.target === saveModal) {saveModal.style.display = "none";}
     if (event.target === loadModal) {loadModal.style.display = "none";}
+    if (event.target === resetModal) {resetModal.style.display = "none";}
 }
 
 
@@ -91,3 +95,19 @@ loadFileInput.addEventListener("change", function(event){
     };
     reader.readAsText(loadFileInput.files[0]);
 }, false);
+
+// Reset save state when user clicks on Confirm reset button
+function resetSaveState() {
+    let solved = document.querySelectorAll('[id=solved]');
+    let portrait = document.querySelectorAll('[class=selected-portrait]');
+    let fate = document.querySelectorAll('[id=fate]');
+
+    for (let i = 0; i < 60; i++) {
+        solved[i].checked = false;
+        portrait[i].src = 'img/portraits/0.webp';
+        fate[i].value = '';
+    }
+
+    resetModal.style.display = "none";
+    saveLocalStorage();
+}
